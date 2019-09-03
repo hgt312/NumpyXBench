4f719f659034f9d5f727999074c465c144f47ce6
Huang, Guangtai <guangtai@amazon.com>
2019-09-03 16:49:25 +0800

    add device switcher rendering in sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -97,7 +97,10 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {
+   '**': ['localtoc.html', 'relations.html', 'sourcelink.html',
+          'searchbox.html', 'versions.html']
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
