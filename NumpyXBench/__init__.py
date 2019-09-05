0581caf2810faa3c57975968784c3a81469a93a8
Huang, Guangtai <guangtai@amazon.com>
2019-09-05 15:22:23 +0800

    fix little bug

--- NumpyXBench/__init__.py
+++ NumpyXBench/__init__.py
@@ -8,12 +8,11 @@ except Exception:
     print("Cannot use MXNet!")
 
 try:
-    os.environ["JAX_PLATFORM_NAME"] = "cpu"
+    # os.environ["JAX_PLATFORM_NAME"] = "cpu"
     os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
     os.environ["JAX_ENABLE_X64"] = "true"
     import jax
     print("Enable x64 for JAX")
-    # print(jax.numpy.ones((1,), dtype='float64').dtype)
 except Exception:
     print("Cannot use jax!")
 
