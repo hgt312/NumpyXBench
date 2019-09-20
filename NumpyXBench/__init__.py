f7b5caa32613898370fd3cd4d8e953d488e75485
Huang, Guangtai <guangtai@amazon.com>
2019-09-20 13:18:35 +0800

    revert something

--- NumpyXBench/__init__.py
+++ NumpyXBench/__init__.py
@@ -1,9 +1,11 @@
 import os
 
 os.environ["MXNET_ENGINE_TYPE"] = "NaiveEngine"
+import mxnet
 
 # os.environ["JAX_PLATFORM_NAME"] = "cpu"
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 os.environ["JAX_ENABLE_X64"] = "true"
+import jax
 
 __version__ = '0.0.5'
